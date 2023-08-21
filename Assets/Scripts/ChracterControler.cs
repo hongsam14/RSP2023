9a536f428fc8fb85ccfe0dc31e1f4fcbd0915253
hongsam14 <ghdtjdgus14@gmail.com>
2023-08-21 07:22:59 -0600

    add character control file

--- Assets/Scripts/ChracterControler.cs
+++ Assets/Scripts/ChracterControler.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class ChracterControler : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private float speed = 0.0f;
+    [SerializeField] private float maxSpeed = 0.0f;
+    [SerializeField] private float jumpForce = 0.0f;
+
+    private Rigidbody2D rigidbody;
+    //status
+    private bool isMoving = false;
+    private bool isJumping = false;
+
+    //input
+    private Vector2 movementInput = Vector2.zero;
+    private bool jumpInput = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //update
+        //horizontal
+        movementInput.x = Input.GetAxis("Horizontal");
+        //jump
+        if (!isJumping && Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpInput = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        moveHorizontal();
+    }
+
+    void moveHorizontal()
+    {
+        rigidbody.velocity += movementInput * speed *  Time.deltaTime;
+        movementInput.x = 0;
     }
 }
